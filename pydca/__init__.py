c1774770caeff6ad9d5176a59d162592ff6988fb
Carlos Bueno <cabb99@gmail.com>
2022-10-30 02:46:44 -0500

    Adds actions

--- pydca/__init__.py
+++ pydca/__init__.py
@@ -1,9 +1,9 @@
-from pydca.plmdca import plmdca
-from pydca.meanfield_dca import meanfield_dca
-from pydca.sequence_backmapper import sequence_backmapper
-from pydca.msa_trimmer import msa_trimmer
-from pydca.contact_visualizer import contact_visualizer
-from pydca.dca_utilities import dca_utilities
+from .plmdca import plmdca
+from .meanfield_dca import meanfield_dca
+from .sequence_backmapper import sequence_backmapper
+from .msa_trimmer import msa_trimmer
+from .contact_visualizer import contact_visualizer
+from .dca_utilities import dca_utilities
 
 """pydca is python implementation of Direct Coupling Analysis for protein and RNA 
 sequences. It implements two flavors of DCA: mean-field and pseudolikelihood maximization.
